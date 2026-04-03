4d19e83e2f13e59111a5ed3fe9c1b8b08971fed9
zelie08 <zelie.drausin@gmail.com>
2026-04-03 15:09:00 +0200

    Update HabitudeClient.java

---
+++ HabitudeClient.java
@@ -0,0 +1,15 @@
+public class HabitudeClient {
+    private int nbLocationsTotales;
+    private double totalDepense;
+    private String categorieClient;
+
+    public void incrementerLocations() {
+        this.nbLocationsTotales++;
+    }
+
+    public void actualiserCategorie() {
+        if (nbLocationsTotales > 10) this.categorieClient = "VIP";
+        else this.categorieClient = "Standard";
+    }
+
+}
