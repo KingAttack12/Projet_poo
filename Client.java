e2f8f8fa98f3b9ec20bd254f224f065630cf2bdc
zelie08 <zelie.drausin@gmail.com>
2026-04-03 17:08:11 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -9,15 +9,17 @@ public class Client {
     private Login login;
     private HabitudeClient habitude;
 
-    public Client(String nom, String prenom, int id_client) {
+    public Client(String nom, String prenom, int id_client, String telephone, String e_mail) {
         this.nom = nom;
         this.prenom = prenom;
         this.id_client = id_client;
+        this.telephone = telephone;
+        this.e_mail = e_mail;
+
     }
 
     public String getInfosClient() {
-        return "Client N°" + id_client + " : " + nom + " " + prenom; 
-
+        return "Client N°" + id_client + " : " + nom + " " + prenom + " " + telephone + " " + e_mail;
     }
 
     public void modifierContact(String tel, String email) {
