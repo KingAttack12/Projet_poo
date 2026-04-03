0076f065bf8248b4bcdbc851f46cf0a9771bd96a
zelie08 <zelie.drausin@gmail.com>
2026-04-03 16:45:36 +0200

    Update HabitudeClient.java

--- HabitudeClient.java
+++ HabitudeClient.java
@@ -9,7 +9,7 @@ public class HabitudeClient {
 
     public void actualiserCategorie() {
         if (nbLocationsTotales > 10 || totalDepense > 1000) this.categorieClient = "VIP";
-        // si le nombre de locations du client est supérieur à dix, alors le client est catégorisé comme VIP
+        // si le nombre de locations du client est supérieur à dix ou que le montant dépensé est supérieur à mille euros, alors le client est catégorisé comme VIP
         else this.categorieClient = "Standard";
         // sinon, il est considéré comme un client standard
     }
