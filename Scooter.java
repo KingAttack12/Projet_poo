91c8b34d20eaeb9a99b5b4f34f8d1288dfbb0f8a
Arrassi Lyes <149335487+KingAttack12@users.noreply.github.com>
2026-04-03 15:10:31 +0200

    change 15:10, 03/04

--- Scooter.java
+++ Scooter.java
@@ -1,38 +1,38 @@
 public class Scooter {
-    	private String id;
-    	private double kilometrage;
-    	private boolean estDisponible;
-    	private Modele modele; 
+    private String id;
+    private double kilometrage;
+    private boolean estDisponible;
+    private Modele modele; 
     
-    	public Scooter(String id, double kilometrage, Modele modele) {
-        	this.id = id;
-        	this.kilometrage = kilometrage;
-        	this.estDisponible = true; // disponible par défaut
-        	this.modele = modele;
-    	}
+    public Scooter(String id, double kilometrage, Modele modele) {
+        this.id = id;
+        this.kilometrage = kilometrage;
+        this.estDisponible = true; // disponible par défaut
+        this.modele = modele;
+    }
     
-    	public void louer() {
-        	this.estDisponible = false;
-    	}
+    public void louer() {
+        this.estDisponible = false;
+    }
     
-    	public void retour(double kmParcourus) {
-        	this.estDisponible = true;
-        	this.kilometrage += kmParcourus;
-    	}
+    public void retour(double kmParcourus) {
+        this.estDisponible = true;
+        this.kilometrage += kmParcourus;
+    }
 
-    	public String getId() {
-        	return id;
-    	}
+    public String getId() {
+        return id;
+    }
 
-    	public double getKilometrage() {
-        	return kilometrage;
-    	}
+    public double getKilometrage() {
+        return kilometrage;
+    }
 
-    	public boolean isEstDisponible() {
-        	return estDisponible;
-    	}
+    public boolean isEstDisponible() {
+        return estDisponible;
+    }
 
-    	public Modele getModele() {
-        	return modele;
-    	}
+    public Modele getModele() {
+        return modele;
+    }
 }
