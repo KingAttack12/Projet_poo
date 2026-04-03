4d492e88ad5d73a522d96582673383f8ae34cf23
zelie08 <zelie.drausin@gmail.com>
2026-04-03 16:36:46 +0200

    Update Login.java

--- Login.java
+++ Login.java
@@ -8,13 +8,22 @@ public class Login {
     }
 
     public boolean verifierConnexion(String id, String mdp) {
+        // si le mot de passe et l'identifié donné sont correct, alors l'accès est autorisé
+        // sinon, le système refuse l'accès et reviens au départ
         if (this.id.equals(id) && this.mdp.equals(mdp)) {
             System.out.print("Accès autorisé");
             return true;
         }
+        else {
+            System.out.print("Accès non autorisé");
+            return false;
+        }
+        }
     }
 
-    public void changerMotDePasse() {
-        
+    public void changerMotDePasse(String nouveauMdp) {
+        // on change le mot de passe pour un nouveau
+        this.mot_de_passe = nouveauMdp;
+        System.out.print("Le mot de passe a été modifié");
     }
 }
