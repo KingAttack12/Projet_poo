fab3d690e9f012b083f515c1127fda881b653787
zelie08 <zelie.drausin@gmail.com>
2026-04-24 10:00:01 +0200

    Update Scooter.java

--- Scooter.java
+++ Scooter.java
@@ -2,37 +2,39 @@ public class Scooter {
     private String id;
     private double kilometrage;
     private boolean estDisponible;
-    private Modele modele;
+    private Modele modele; // référence à la classe modèle
     
+    
+    // Méthode pour créer un nouveau scooter 
     public Scooter(String id, double kilometrage, Modele modele) {
-        this.id = id;
+        this.id = id; // on enregristre les différente données
         this.kilometrage = kilometrage;
         this.estDisponible = true; // disponible par défaut
         this.modele = modele;
     }
     
-    public void louer() {
-        this.estDisponible = false;
+    public void louer() { // quand on loue le scooter
+        this.estDisponible = false; // il n'est plus disponible
     }
     
-    public void retour(double kmParcourus) {
-        this.estDisponible = true;
-        this.kilometrage += kmParcourus;
+    public void retour(double kmParcourus) { // quand on a de nouveau le scooter
+        this.estDisponible = true; // il redevient disponible
+        this.kilometrage += kmParcourus; // on rajoute les kilomètres parcourus
     }
 
     public String getId() {
-        return id;
+        return id; // donne l'id du scooter
     }
 
     public double getKilometrage() {
-        return kilometrage;
+        return kilometrage; // donne le kilométrage actuel
     }
 
     public boolean isEstDisponible() {
-        return estDisponible;
+        return estDisponible; // donne si le scooter est libre
     }
 
     public Modele getModele() {
-        return modele;
+        return modele; // donne les détails du modèle
     }
 }
