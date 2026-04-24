12ab44ca609fbaa54f27e706b3cca62e7ad5d8b0
zelie08 <zelie.drausin@gmail.com>
2026-04-24 09:50:02 +0200

    Update Tarification.java

--- Tarification.java
+++ Tarification.java
@@ -1,22 +1,23 @@
 public class Tarification {
-    private double prixJournalierBase;
+    private double prixJournalierBase; // variable qui stocke le prix
     
     // Constructeur pour définir le prix de départ
-    public Tarification(double prixInitial) {
-        this.prixJournalierBase = prixInitial;
+    public Tarification(double prixInitial) { // on reçoit un prix de départ
+        this.prixJournalierBase = prixInitial; // on enregistre ce prix 
     }
     
-    public void modifierPrixBase(double nouveauPrix) {
-        this.prixJournalierBase = nouveauPrix;
-        System.out.println("Le nouveau prix de base est de : " + this.prixJournalierBase + " euro");
+    public void modifierPrixBase(double nouveauPrix) { // on reçoit un nouveau prix
+        this.prixJournalierBase = nouveauPrix; // on remplace l'ancien prix par le nouveau prix
+        System.out.println("Le nouveau prix de base est de : " + this.prixJournalierBase + " euro"); // on l'affiche à l'écran
     }
 
     // Méthode pour calculer le prix selon la durée (utilisée par le Contrat)
-    public double calculerTarifBase(int nbJours) {
-        return this.prixJournalierBase * nbJours;
+    public double calculerTarifBase(int nbJours) { // on a un nombre de jours
+        return this.prixJournalierBase * nbJours; // on fait la multiplication et on donne le résultat
     }
 
-    public double getPrixJournalierBase() {
-        return prixJournalierBase;
+    // Méthode pour lire le prix
+    public double getPrixJournalierBase() { // on demande le prix actuel
+        return prixJournalierBase; // on répond la valeur qui est stockée en mémoire
     }
 }
